60894238f6d2f25458e177451df44d75a52fe27b
alekhya <arvapallialekhya571@gmail.com>
2024-06-28 00:35:36 +0530

    Initial commit

---
+++ src/components/CodeEditor.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Prism from 'prismjs';
+import 'prismjs/themes/prism.css';
+import './CodeEditor.css';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { vs } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+const CodeEditor = () => {
+    const [code, setCode] = useState('');
+
+    const handleChange = (event) => {
+        setCode(event.target.value);
+    };
+
+    return (
+        <div className="editor-container">
+            <textarea
+                value={code}
+                onChange={handleChange}
+                className="code-input"
+                spellCheck="false"
+            />
+            <div className="code-output">
+                <SyntaxHighlighter language="javascript" style={vs}>
+                    {code}
+                </SyntaxHighlighter>
+            </div>
+        </div>
+    );
+};
+
+export default CodeEditor;
